8482cb28ec568369f4d7ce5880c7e2b93467aab1
fryarludwig <admin@fryarludwig.com>
2015-09-23 01:00:29 -0600

    Added JS to allow saved searches (locally), add saved searches to active filter list, added JS for each search term for pinning

--- resources/js/searchFunctions.js
+++ resources/js/searchFunctions.js
@@ -1,27 +1,86 @@
 var savedSearchesList = [];
+var currentFilterList = [];
 
-function pinSearchCritera(){
-  savedSearchesList.push(document.getElementById("captionText").value);
-  var htmlText = "";
-  var opening = "<li><button>X</button><a>";
+document.addEventListener('DOMContentLoaded', function() {
+    loadSavedSearches();
+}, false);
+
+function loadSavedSearches() {
+  savedSearchesList = JSON.parse(localStorage.getItem("savedSearchesList"));
+  updatePinnedSearches();
+}
+
+function addToCurrentFilters(index) {
+  currentFilterList.push(savedSearchesList[index]);
+  updateCurrentFilters();
+}
+
+function removeCurrentFilter(index) {
+  currentFilterList.splice(index, 1);
+  updateCurrentFilters();
+}
+
+function updateCurrentFilters() {  var htmlText = "";
+  var searchId = currentFilterList.length;
+  var opening = "<li><button onclick=\"removeCurrentFilter(";
+  var middle = ")\"> X</button><a>";
   var closing = "</a></li>";
-  for (i = 0; i < savedSearchesList.length; i++) {
-    htmlText += opening + savedSearchesList[i] + closing;
+  for (i = 0; i < currentFilterList.length; i++) {
+    htmlText += opening + i + middle + currentFilterList[i] + closing;
   }
-  document.getElementById("pinnedSearchItems").innerHTML = htmlText;
+  document.getElementById("currentSearchFilters").innerHTML = htmlText;
 }
 
 function pinHashtag(){
-  savedSearchesList.push("#" + document.getElementById("hashtagText").value);
+  if (document.getElementById("hashtagText").value) {
+    savedSearchesList.push(document.getElementById("hashtagText").value);
+    updatePinnedSearches();
+  }
+}
+
+function pinCaption(){
+  if (document.getElementById("captionText").value) {
+    savedSearchesList.push(document.getElementById("captionText").value);
+    updatePinnedSearches();
+  }
+}
+
+function pinComment(){
+  if (document.getElementById("commentText").value) {
+    savedSearchesList.push(document.getElementById("commentText").value);
+    updatePinnedSearches();
+  }
+}
+
+function pinDate(){
+  if (document.getElementById("dateText").value) {
+    savedSearchesList.push(document.getElementById("dateText").value);
+    updatePinnedSearches();
+  }
+}
+
+function pinLocation(){
+  if (document.getElementById("locationText").value) {
+    savedSearchesList.push(document.getElementById("locationText").value);
+    updatePinnedSearches();
+  }
+}
+
+function removeSavedTerm(index) {
+  savedSearchesList.splice(index, 1);
   updatePinnedSearches();
 }
 
 function updatePinnedSearches(){
   var htmlText = "";
-  var opening = "<li><button>X</button><a>";
+  var searchId = savedSearchesList.length;
+  var opening = "<li><button onclick=\"removeSavedTerm(";
+  var middle = ")\"> X</button><a onclick=\"addToCurrentFilters(";
+  var nextMiddle = ")\">";
   var closing = "</a></li>";
   for (i = 0; i < savedSearchesList.length; i++) {
-    htmlText += opening + savedSearchesList[i] + closing;
+    htmlText += opening + i + middle + i + nextMiddle + savedSearchesList[i] + closing;
   }
   document.getElementById("pinnedSearchItems").innerHTML = htmlText;
+  localStorage.setItem("savedSearchesList", JSON.stringify(savedSearchesList));
 }
